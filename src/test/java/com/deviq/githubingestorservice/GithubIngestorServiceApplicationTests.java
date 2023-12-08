03587bb72b80c7a4d1abfa55d118fe48ab80a546
naveen9513 <naveenavishkauom@gmail.com>
2023-12-08 09:23:25 +0530

    create project

---
+++ src/test/java/com/deviq/githubingestorservice/GithubIngestorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deviq.githubingestorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIngestorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
