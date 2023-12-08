b98db8951e0c2b339654605c49268c1cf8e52e99
naveen9513 <naveenavishkauom@gmail.com>
2023-12-08 16:03:40 +0530

    add rest controller

---
+++ src/main/java/com/example/githubingestorservice/Controller.java
@@ -0,0 +1,13 @@
+package com.example.githubingestorservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
+    }
+}
