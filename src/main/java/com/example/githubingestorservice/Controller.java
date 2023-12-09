97693871ed85889b8dbea3fcb1853860ecdfb424
naveen9513 <naveenavishkauom@gmail.com>
2023-12-09 16:03:51 +0530

    name change

--- src/main/java/com/example/githubingestorservice/Controller.java
+++ src/main/java/com/example/githubingestorservice/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @GetMapping("/welcome12")
+    @GetMapping("/welcome")
     public String welcome() {
         return "welcome";
     }
