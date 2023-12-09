5cfd0ca7e65f85d9fcd081b5e293dfee8ad7343d
naveen9513 <naveenavishkauom@gmail.com>
2023-12-09 15:53:26 +0530

    test

--- src/main/java/com/example/githubingestorservice/Controller.java
+++ src/main/java/com/example/githubingestorservice/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @GetMapping("/welcome")
+    @GetMapping("/welcome12")
     public String welcome() {
         return "welcome";
     }
