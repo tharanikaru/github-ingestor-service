54955488fcc106198d21bca9b70070cdb7cfee13
naveen9513 <naveenavishkauom@gmail.com>
2023-12-10 17:51:24 +0530

    add logs

--- src/main/java/com/tharani/githubingestorservice/controller/MetricController.java
+++ src/main/java/com/tharani/githubingestorservice/controller/MetricController.java
@@ -4,6 +4,7 @@ import com.tharani.githubingestorservice.dto.AddDeveloperRequest;
 import com.tharani.githubingestorservice.dto.AddDeveloperResponse;
 import com.tharani.githubingestorservice.service.MetricService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j(topic = "[MetricController]")
 @RequestMapping("/api/v1/metric")
 public class MetricController {
 
@@ -18,6 +20,7 @@ public class MetricController {
 
     @PostMapping("/addDeveloper")
     public AddDeveloperResponse addDeveloper(@RequestBody AddDeveloperRequest request) {
+        log.info("request received : {}", request);
         return metricService.addDeveloper(request);
     }
 }
--- src/main/java/com/tharani/githubingestorservice/dto/AddDeveloperRequest.java
+++ src/main/java/com/tharani/githubingestorservice/dto/AddDeveloperRequest.java
@@ -1,14 +1,12 @@
 package com.tharani.githubingestorservice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class AddDeveloperRequest {
     private String firstName;
     private String lastName;
